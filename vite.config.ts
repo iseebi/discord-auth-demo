3de4afa7fc1053e172906f7b1fcca25f4546fa3f
Nobuhiro Ito <iseebi@iseteki.net>
2024-04-21 13:37:54 +0900

    configure https

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import * as fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '127.0.0.1',  // IPv4での起動を強制する
+    port: 6200, // ポートがずれるとOAuthの設定が手間になるので、起動ポートも固定しておく
+    https: {
+      key: fs.readFileSync("certs/key.pem"),
+      cert: fs.readFileSync("certs/cert.pem"),
+    },
+  },
 })
