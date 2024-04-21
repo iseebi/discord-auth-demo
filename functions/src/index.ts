a97de832dcae15e0c6014ffa9d92640363c335bf
Nobuhiro Ito <iseebi@iseteki.net>
2024-04-21 18:32:31 +0900

    implement api

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -9,14 +9,18 @@
 
 import {onRequest} from "firebase-functions/v2/https";
 import {defineString} from "firebase-functions/params";
+import * as admin from "firebase-admin";
 import {AppSecretConfigHandler} from "./secretConfig";
 // import * as logger from "firebase-functions/logger";
 
 const region = "asia-northeast1";
 const host = defineString("APP_HOST");
+const requestScopes = ["identify", "guilds"];
 
 // APP_SECRET_CONFIG を取得して保持する
 const appSecretConfig = new AppSecretConfigHandler();
+
+admin.initializeApp();
 
 // noinspection JSUnusedGlobalSymbols
 export const httpHandler = onRequest({
@@ -25,17 +29,80 @@ export const httpHandler = onRequest({
 }, async (request, response) => {
   const path = request.path;
 
-  // const redirectUri = `https://${host}/auth/discord/completion`;
+  const secretConfig = appSecretConfig.get();
+  const redirectUri = `https://${host.value()}/auth/discord/completion`;
 
-  logger.info(`Request path: https://${host}${path}`);
+  switch (path) {
+  case "/api/auth/discord": {
+    const scopesString = requestScopes.join(" ");
+    response.send({
+      authorizeUri: "https://discord.com/api/oauth2/authorize?" +
+          `client_id=${secretConfig.discordClientId}` +
+          `&redirect_uri=${redirectUri}` +
+          `&response_type=code&scope=${encodeURIComponent(scopesString)}`,
+    });
+    break;
+  }
+  case "/api/auth/discord/completion": {
+    // code を得る
+    const code = request.body.code;
+    if (!code) {
+      response.status(400).send("Bad Request");
+      return;
+    }
 
-  response.send({
-    message: "Hello from Firebase!",
-    host: host.value(),
-    path,
-    headers: request.headers,
-    env: process.env,
-    isFirebaseCli: process.env.FUNCTIONS_EMULATOR === "true",
-    discordClientId: appSecretConfig.get().discordClientId, // 例としてClient IDを返す
-  });
+    // Discord からトークンを得る
+    const authResult = await fetch("https://discord.com/api/oauth2/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: new URLSearchParams({
+        client_id: secretConfig.discordClientId,
+        client_secret: secretConfig.discordClientSecret,
+        grant_type: "authorization_code",
+        code,
+        redirect_uri: redirectUri,
+      }),
+    });
+    const authJson = await authResult.json() as {
+        access_token: string,
+        refresh_token: string,
+        error?: string
+      };
+    if (authJson.error) {
+      response.status(500).send(authJson.error);
+      return;
+    }
+    const accessToken = authJson.access_token;
+    // const refreshToken = authJson.refresh_token;
+
+    // Discord からユーザー情報を得る
+    const userResult = await fetch("https://discord.com/api/users/@me", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    });
+    const userJson = await userResult.json() as {
+        id: string,
+        username: string,
+      };
+
+    // Firebase Authentication でカスタムトークンを発行する
+    const token = await admin.auth().createCustomToken(
+      `discord:${userJson.id}`,
+      {
+        "discord:id": userJson.id,
+        "discord:username": userJson.username,
+      });
+
+    // TODO: 必要があれば、ここで accessToken/refreshToken を保存する
+
+    response.send({token});
+    break;
+  }
+  default:
+    response.status(404).send("Not Found");
+    break;
+  }
 });
